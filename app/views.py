d40079a314139dd64f6ebc2c6511db2e67c2b711
EuSolluna <sol.luna@escolar.ifrn.edu.br>
2023-12-14 16:56:18 -0300

    envio

--- app/views.py
+++ app/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import LoginForm
 
-# Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')  # Substitua 'home' pelo nome da sua página inicial
+    else:
+        form = LoginForm()
+
+    return render(request, 'login.html', {'form': form})
